a318f4f24c9a385b940aeedcf14ecd36499ca5db
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-09-02 03:32:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-let submitData = {
-  name: name,
-  email: email
-};
+function submitData(name, email) = {
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({name: name, email: email})
+  };
+
+fetch("http://localhost:3000/users", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    console.log(object);
+  })
+  .catch(function(error) {
+    alert("Bad things! Ragnarők!");
+    console.log(error.message);
+  });
